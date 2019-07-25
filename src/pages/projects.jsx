9c34475399cae1e61cfeeb327f9755217d664411
Mike Barkmin <mike.barkmin@uni-due.de>
2019-07-25 10:35:57 +0200

    Fix empty file

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 function Projects() {
   return <div></div>;
 }
-
-Projects.propTypes = {};
 
 export default Projects;
--- src/pages/publications.jsx
+++ src/pages/publications.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 function Publications() {
   return <div></div>;
 }
-
-Publications.propTypes = {};
 
 export default Publications;
