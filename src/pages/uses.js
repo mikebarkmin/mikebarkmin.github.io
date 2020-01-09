cc353f997a1e076669d2dc39d2aff81c121c4c74
Mike Barkmin <mike.barkmin@uni-due.de>
2020-01-09 16:56:58 +0100

    Create uses.js

---
+++ src/pages/uses.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Uses() {
+  return <div>Mostly zsh and neovim. You can find my dotfiles <a href="https://github.com/mikebarkmin/dotfiles">here</a></div>;
+}
+
+NotFound.propTypes = {};
+
+export default Uses;
