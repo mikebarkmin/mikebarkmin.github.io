2ac291c8473ea4c13c0ac2ed28019a5fb589027b
Mike Barkmin <mike.barkmin@uni-due.de>
2021-12-08 23:13:12 +0100

    update copyright year

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ const Wrapper = styled.footer`
 function Footer() {
   return (
     <Wrapper>
-      &copy; 2019 by Mike Barkmin.{' '}
+      &copy; 2021 by Mike Barkmin.{' '}
       <a
         rel="noreferrer"
         href="https://github.com/mikebarkmin/mikebarkmin.github.io"
